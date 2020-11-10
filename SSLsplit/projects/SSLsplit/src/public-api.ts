9360d0504f27e4edc80bf9f47a82daefceffeb1d
Father-Stalin <46010693+Father-Stalin@users.noreply.github.com>
2020-11-10 07:58:32 -0600

    Removed SSLsplit
    Due to constraints on certain libraries and not being able to use mitmproxy or sslsplit, I'll be messing with stuff offline

--- SSLsplit/projects/SSLsplit/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of SSLsplit
- */
-
-export * from './lib/services/SSLsplit.service';
-export * from './lib/components/SSLsplit.component';
-export * from './lib/SSLsplit.module';
