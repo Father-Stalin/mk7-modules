53d1522d0f1720806020dd732b688cf235582cc9
Father-Stalin <46010693+Father-Stalin@users.noreply.github.com>
2020-11-10 22:41:21 -0600

    figured out polling for process

--- speedtest/projects/speedtest/src/module.py
+++ speedtest/projects/speedtest/src/module.py
@@ -9,22 +9,25 @@ from pineapple.modules import Module, Request
 module = Module('speedtest',logging.DEBUG)
 
 global process
-process = 0
+process = subprocess.Popen("echo")
 
 def _check_speedtest_running():
-    if process == None:
+    if process.poll() == None:
         return 1
     else:
         return 0
 
 @module.handles_action('run_test')
 def run_test(request: Request):
+ # debug, we may need job control bc timeout is 6 secs
     sleep(2)
     return "nice"
     downloadCMD = "wget -O /dev/null http://cachefly.cachefly.net/50mb.test"
     downloadCMD = downloadCMD.split(" ")
     startTime = time.time()
-    subprocess.Popen(downloadCMD, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
+    global process
+    process = subprocess.Popen(downloadCMD, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    process.wait()
     duration = time.time() - startTime
     duration = round(duration,4)
     rate = 50 / duration
